1c014fac3aa8ff7d209cbbab7e1499466a9e8e80
Jey <jey@Jeys-MBP.home>
2020-06-14 17:48:40 -0400

    modified baste test and testngregression test

--- src/main/java/com/qa/base/BaseTest.java
+++ src/main/java/com/qa/base/BaseTest.java
@@ -5,27 +5,27 @@ import java.util.Properties;
 import org.openqa.selenium.WebDriver;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Parameters;
 
 import com.qa.hubspot.pages.LoginPage;
 
 public class BaseTest {
 
 
-
-
 	public WebDriver driver; 
-	
+
 	public BasePage basepage;
 	public LoginPage loginPage;
 	public Properties prop;
-	
 
 
+	@Parameters("browser")
 	@BeforeTest
-	public void setUp() {
+	public void setUp(String browserName) {
 
 		basepage = new BasePage();
 		prop = basepage.init_prop();
+		prop.setProperty("browser", browserName);
 		driver = basepage.init_driver(prop);
 		loginPage = new LoginPage(driver);
 
